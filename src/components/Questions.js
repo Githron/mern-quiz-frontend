8b3183bfc0d9a141b642d7cfc558541719014583
githron <allewnor@gmail.com>
2023-03-09 16:17:52 +0000

    init

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -15,80 +15,101 @@ const Questions = () => {
     const [hasAnswered, setHasAnswered] = useState(false);
     const navigate = useNavigate();
 
-    const handleAnswerOptionClick = (option, optionNumber) => {
-      if (!disableDiv && !hasAnswered) {
+    const handleAnswerOptionClick = (option, optionNumber, event) => {
+        if (!disableDiv && !hasAnswered) {
           setSelectedAnswer({ text: option, number: optionNumber });
           setDisableDiv(true);
           setHasAnswered(true);
           const nextDiv = document.getElementById("next-question");
           nextDiv.style.display = "block";
-  
+      
           const correctAnswer = quiz_questions[currentQuestion].answer;
           console.log(
-              ` ${name} Question ${currentQuestion + 1}: Answer Selected: ${
-                  optionNumber + 1
-              }. ${option}, Correct Answer: ${correctAnswer}`
+            `${name} Question ${currentQuestion + 1}: Answer Selected: ${
+              optionNumber + 1
+            }. ${option}, Correct Answer: ${correctAnswer}`
           );
-  
+      
           if (optionNumber + 1 === correctAnswer) {
-              setTotalCorrectAnswers(totalCorrectAnswers + 1);
-              console.log(`Selected answer is correct!`);
+            setTotalCorrectAnswers(totalCorrectAnswers + 1);
+            console.log(`Selected answer is correct!`);
+            // Style the selected answer as green if it is correct
+            event.target.style.backgroundColor = "green";
           } else {
-              console.log(`Selected answer is incorrect!`);
+            console.log(`Selected answer is incorrect!`);
+            // Style the selected answer as red if it is incorrect
+            event.target.style.backgroundColor = "red";
+      
+            // Style the correct answer as green
+            const options = event.target.parentElement.children;
+            for (let i = 0; i < options.length; i++) {
+              const optionElem = options[i];
+              const optionNumber = i + 1;
+              if (optionNumber === correctAnswer) {
+                optionElem.style.backgroundColor = "green";
+                break;
+              }
+            }
           }
-  
+      
           clearInterval(countdownId); // Stop the countdown timer
-      }
-  };
-  
-
-    const handleNextQuestionClick = () => {
-      setCount(10)
-        if (currentQuestion === quiz_questions.length - 1) {
-            // Last question, redirect to quiz-result page
-            // console.log("quiz result");
-            navigate("/quiz-result", {
-                state: {
-                    totalQuestions: totalQuestions,
-                    totalCorrectAnswers: totalCorrectAnswers,
-                    name: name,
-                },
-
-                
-            });
-            return;
         }
+      };
+      
+      
+      
 
+      const handleNextQuestionClick = () => {
+        setCount(10);
+        if (currentQuestion === quiz_questions.length - 1) {
+          // Last question, redirect to quiz-result page
+          navigate("/quiz-result", {
+            state: {
+              totalQuestions: totalQuestions,
+              totalCorrectAnswers: totalCorrectAnswers,
+              name: name,
+            },
+          });
+          return;
+        }
+      
+        // Reset the background color of all the options
+        const options = document.querySelectorAll(".option");
+        options.forEach((option) => {
+          option.style.backgroundColor = "";
+        });
+      
         setCurrentQuestion(currentQuestion + 1);
         setSelectedAnswer(null);
         setDisableDiv(false);
         setHasAnswered(false);
         const nextDiv = document.getElementById("next-question");
         nextDiv.style.display = "none";
-    };
+      };
+      
+      
 
     const [count, setCount] = useState(10);
     const [countdownId, setCountdownId] = useState(null);
 
     useEffect(() => {
-      const countdown = setInterval(() => {
-          if (count > 0) {
-              setCount(count - 1);
-          } else {
-              clearInterval(countdown);
-              console.log("Time's up!");
-              setDisableDiv(true);
-              const nextDiv = document.getElementById("next-question");
-              nextDiv.style.display = "block";
-          }
-      }, 1000);
-  
-      // Store the countdown ID in a state variable
-      setCountdownId(countdown);
-  
-      return () => clearInterval(countdown);
-  }, [count]);
-  
+        const countdown = setInterval(() => {
+            if (count > 0) {
+                setCount(count - 1);
+            } else {
+                clearInterval(countdown);
+                console.log("Time's up!");
+                setDisableDiv(true);
+                const nextDiv = document.getElementById("next-question");
+                nextDiv.style.display = "block";
+            }
+        }, 1000);
+
+        // Store the countdown ID in a state variable
+        setCountdownId(countdown);
+
+        return () => clearInterval(countdown);
+    }, [count]);
 
     return (
         <div id="container">
@@ -118,8 +139,12 @@ const Questions = () => {
                                             : "auto",
                                     }}
                                     key={index}
-                                    onClick={() =>
-                                        handleAnswerOptionClick(option, index)
+                                    onClick={(event) =>
+                                        handleAnswerOptionClick(
+                                            option,
+                                            index,
+                                            event
+                                        )
                                     }
                                 >
                                     {`${option}`}
