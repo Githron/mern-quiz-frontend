9e8a05f401421fa5581160bb2a0473f23cc7263b
githron <allewnor@gmail.com>
2023-03-09 18:56:16 +0000

    init

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -60,7 +60,7 @@ const Questions = () => {
       
 
       const handleNextQuestionClick = () => {
-        setCount(10);
+        setCount(20);
         if (currentQuestion === quiz_questions.length - 1) {
           // Last question, redirect to quiz-result page
           navigate("/quiz-result", {
@@ -89,7 +89,7 @@ const Questions = () => {
       
       
 
-    const [count, setCount] = useState(10);
+    const [count, setCount] = useState(20);
     const [countdownId, setCountdownId] = useState(null);
 
     useEffect(() => {
